f8c25b4431b0e1970360da7ffa665836ede79819
Yehyv <101909827+Yehyv@users.noreply.github.com>
2023-08-18 17:02:50 +0300

    xxx

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -44,7 +44,7 @@ class BlogPostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+     public function store(Request $request)
     {
         $newPost = BlogPost::create([
             'title'=>$request->title,
